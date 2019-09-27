49e27e61d43884cde8a63712749a03bc71c487b3
Tymur <tymur.lysenko@gmail.com>
2019-09-27 21:02:38 +0300

    DB models: events and projects

--- backend/app/app/db_models/models.py
+++ backend/app/app/db_models/models.py
@@ -18,6 +18,9 @@ class Organizer(Base):
 
     invites = relationship("Invite", back_populates='issued_by')
 
+    projects = relationship("OrganizerProject", back_populates="organizer")
+    events = relationship("OrganizerEvent", back_populates="organizer")
+
 
 class Invite(Base):
     __tablename__ = 'invites'
@@ -27,10 +30,10 @@ class Invite(Base):
     key = Column(String, nullable=False)
     activated = Column(Boolean, default=False)
 
-    invitor_id = Column(Integer, ForeignKey('organizers.id'))
+    invitor_id = Column(Integer, ForeignKey('organizers.id'), nullable=True)
     invitor = relationship("Organizer", back_populates='invited_by')
 
-    issued_by_id = Column(Integer, ForeignKey('organizers.id'))
+    issued_by_id = Column(Integer, ForeignKey('organizers.id'), nullable=False)
     issued_by = relationship('Organizer', back_populates='invites')
 
 
@@ -53,8 +56,8 @@ class QR_data(Base):
 
 volunteer_tag = Table(
     'volunteer_tags', Base.metadata,
-    Column("volunteer_id", Integer, ForeignKey('volunteers.id')),
-    Column('tag_id', Integer, ForeignKey('tags.id')),
+    Column("volunteer_id", Integer, ForeignKey('volunteers.id'), nullable=False),
+    Column('tag_id', Integer, ForeignKey('tags.id'), nullable=False),
 )
 
 
@@ -69,6 +72,7 @@ class Tag(Base):
 
 class Volunteer(Base):
     __tablename__ = 'volunteers'
+
     id = Column(Integer, primary_key=True)
     volunteer_id = Column(String, nullable=False, unique=True)
     vk_id = Column(Integer, nullable=False, unique=True)
@@ -77,11 +81,63 @@ class Volunteer(Base):
     date_of_birth = Column(Date, nullable=True)
     karma = Column(DECIMAL)
 
-    interests = relationship('Tag', secondary=volunteer_tag, backref='volunteers')
+    interests = relationship('Tag', secondary=volunteer_tag, back_populates ='volunteers')
 
 
 class Role(Base):
-    id = Column(Integer, primary_key=True)
+    __tablename__ = "roles"
 
+    id = Column(Integer, primary_key=True)
     description = Column(String, nullable=False)
     name = Column(String, nullable=False)
+
+
+class Event(Base):
+    __tablename__ = "events"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    description = Column(String, nullable=False)
+    base_karma_to_pay = Column(Integer, nullable=False)
+    start_datetime = Column(DateTime, nullable=False)
+    end_datetime = Column(DateTime, nullable=False)
+
+    project_id = Column(Integer, ForeignKey("projects.id"), nullable=True)
+    project = relationship("Project", back_populates="events")
+
+    organizers = relationship("OrganizerEvent", back_populates="event")
+
+
+class OrganizerEvent(Base):
+    __tablename__ = "organizers_events"
+
+    id = Column(Integer, primary_key=True)
+
+    organizer_id = Column(Integer, ForeignKey("organizers.id"), nullable=False),
+    organizer = relationship("Organizer", back_populates="events")
+
+    event_id = Column(Integer, ForeignKey("events.id"), nullable=False)
+    event = relationship("Event", back_populates="organizers")
+
+
+class Project(Base):
+    __tablename__ = "projects"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    description = Column(String, nullable=True)
+
+    organizers = relationship("OrganizerProject", back_populates="project")
+    events = relationship("Event", back_populates="project")
+
+
+class OrganizerProject(Base):
+    __tablename__ = "organizers_projects"
+
+    id = Column(Integer, primary_key=True)
+
+    organizer_id = Column(Integer, ForeignKey("organizers.id"), nullable=False),
+    organizer = relationship("Organizer", back_populates="events")
+
+    project_id = Column(Integer, ForeignKey("projects.id"), nullable=False)
+    project = relationship("Project", back_populates="organizers")
