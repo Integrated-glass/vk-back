d5a54a5e471b7f3761200c8ef23ebdd92e4786ad
WinnerOK <danman339@gmail.com>
2019-09-27 22:38:10 +0300

    changed defaults

--- backend/app/app/db/init_db.py
+++ backend/app/app/db/init_db.py
@@ -9,17 +9,18 @@ from app.db import base
 
 
 def init_db(db_session):
+    pass
     # Tables should be created with Alembic migrations
     # But if you don't want to use migrations, create
     # the tables un-commenting the next line
     # Base.metadata.create_all(bind=engine)
 
-    user = crud.user.get_by_email(db_session, email=config.FIRST_SUPERUSER)
-    if not user:
-        user_in = OrganizerCreate(
-            email=config.FIRST_SUPERUSER,
-            password=config.FIRST_SUPERUSER_PASSWORD,
-            is_superuser=True,
-            full_name="Ivan Ivanov"
-        )
-        user = crud.user.create(db_session, user_in=user_in)
+    # user = crud.user.get_by_email(db_session, email=config.FIRST_SUPERUSER)
+    # if not user:
+    #     user_in = OrganizerCreate(
+    #         email=config.FIRST_SUPERUSER,
+    #         password=config.FIRST_SUPERUSER_PASSWORD,
+    #         is_superuser=True,
+    #         full_name="Ivan Ivanov"
+    #     )
+    #     user = crud.user.create(db_session, user_in=user_in)
