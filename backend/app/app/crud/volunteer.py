b251bb8b4a6c5391cd7a7fce76ade9a48d017ec6
WinnerOK <danman339@gmail.com>
2019-09-28 14:59:22 +0300

    Kostyl year

--- backend/app/app/crud/volunteer.py
+++ backend/app/app/crud/volunteer.py
@@ -1,13 +1,14 @@
 from typing import List, Optional
 
 from sqlalchemy.orm import Session
-
+from datetime import MINYEAR
 from app.db_models.models import VolunteerLogin
 from app.models.models import VolunteerForm
 
 
 def create(db_session: Session, *, user_in: VolunteerForm) -> VolunteerLogin:
     data = dict(user_in)
+    data['date_of_birth'] = f"{data['date_of_birth']}.{MINYEAR}"
     user = VolunteerLogin(
         **data
     )
