baf17f297b7c9a30dd2be643fc7b76be08d32b2e
WinnerOK <danman339@gmail.com>
2019-09-28 14:51:37 +0300

    [insecure] CORS

--- backend/app/app/main.py
+++ backend/app/app/main.py
@@ -1,24 +1,31 @@
 from fastapi import FastAPI
 from starlette.requests import Request
+from starlette.middleware.cors import CORSMiddleware
 
 from app.api.api_v1.api import api_router
 from app.core import config
 from app.db.session import Session
 
 import logging
+
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
 app = FastAPI(title=config.PROJECT_NAME)
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(api_router, prefix=config.API_V1_STR)
 
 
 @app.middleware("http")
-async def db_session_middleware(request: Request, call_next ):
+async def db_session_middleware(request: Request, call_next):
     logger.debug("Entered middleware")
     request.state.db = Session()
     response = await call_next(request)
     request.state.db.close()
     return response
-
